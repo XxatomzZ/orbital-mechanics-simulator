ecbdbec2be84c337ce0822c951fc02f916e6c6b2
XxatomzZ <cam.372@outlook.com>
2026-06-02 00:32:33 +0100

    Set up calculation for mu, wrote time step function with very basic methodology - to be improved later, set basic ICs

--- src/simulation.py
+++ src/simulation.py
@@ -1,5 +1,6 @@
 import numpy as np
 from bodies import bodies
+from scipy import constants
 #from scipy.integrate import solve_ivp
 
 
@@ -13,6 +14,18 @@ r = np.zeros((3, nt))
 v = np.zeros((3, nt))
 
 
+'''
+User will select center of mass and orbiting body here
+For now, I will just test out the interaction between the Moon and Earth
+'''
+
+# gravitational constant G
+G = constants.G
+
+# calculate M and subsequently mu
+M = bodies['Earth']['m'] + bodies['Moon']['m']
+mu = G * M
+
 # set r_x                       ## NEED TO TIDY UP LATER 
 E_r = bodies['Earth']['r']
 M_r = bodies['Moon']['r']
@@ -21,18 +34,33 @@ r_x = E_r + M_r + M_d
 
 #print('r_x:', r_x)
 
-'''
-# set ICs
-r[0,0] = (r_x, 0, 0)                  ## distance from center of mass (c.o.m at origin for simplicity)
-v[0] = (v_x, v_y, v_z)              ## approx. circular velocity
 
+# set ICs
+r[0, 0] = r_x                  ## distance from center of mass (c.o.m at origin for simplicity)
+v[0, 0] = 1                    ## v_x (set as 1 as a placeholder)
+v[1, 0] = 0                    ## v_y
+v[2, 0] = 0                    ## v_z 
 
 
 # simulate the time steps
-for t in range(1, nt-1):
-    a_grv = - ((G * M) / (np.abs(r[t]) ** 3)) * r[t]        ## acc due to gravity
-    a_thr = 0                                               ## a_thr = 0 for natural bodies 
-    v[t+1] = v[t] + (a_grv + a_thr) * dt
-    r[t+1] = r[t] + v[t+1] * dt
-
 '''
+for now everything will be split into x,y,z components
+later I will condense this to store all components in a (3,nt) matrix
+'''
+def time_step(v, r, mu):
+    for t in range(1, nt-1):
+        a_grv_x = - (mu / (np.abs(r[0, t]) ** 3)) * r[0, t]        ## acc. due to gravity    (3 x nt)
+        a_grv_y = - (mu / (np.abs(r[1, t]) ** 3)) * r[1, t]
+        a_grv_z = - (mu / (np.abs(r[2, t]) ** 3)) * r[2, t]
+        a_thr = 0                                               ## a_thr = 0 for natural bodies 
+        
+        # now calculate updated velocities and radii
+        v[0, t+1] = v[0, t] + (a_grv_x + a_thr) * dt
+        v[1, t+1] = v[1, t] + (a_grv_y + a_thr) * dt
+        v[2, t+1] = v[2, t] + (a_grv_z + a_thr) * dt
+        r[0, t+1] = r[0, t] + v[0, t+1] * dt
+        r[1, t+1] = r[1, t] + v[1, t+1] * dt
+        r[2, t+1] = r[2, t] + v[2, t+1] * dt
+
+    return v, r
+
